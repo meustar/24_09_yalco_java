94f01998b2c2ccd74a7b35cd301a2457291f378b
meustar <meustar829@gmail.com>
2024-09-11 23:59:27 +0900

    정수 자료형들과 관련 연산자(상)

---
+++ src/sec03/chap01/Ex01.java
@@ -0,0 +1,11 @@
+package sec03.chap01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        byte    _1b_byte = 1;
+        short   _2b_short = 2;
+        int     _4b_int = 3; // ⭐️ 일반적으로 널리 사용
+        long    _8b_long = 4;
+    }
+}
